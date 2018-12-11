b063be86c8c8fe5e2d4a9fc215d26c1c4f96030c
Enrique Berzosa <eberzosa@gmail.com>
2018-12-11 15:26:43 +0000

    Fixed build

--- build.cmd
+++ build.cmd
@@ -1,17 +1,19 @@
 @SETLOCAL
 
 @SET Root=X:\
-@SET Nuget=%Root%\tools\nuget.exe
-@SET NugetFolder=%Root%\NuGetPackages
-
-
-@SET Solution=src\LightInject.sln
-
-MsBuild.exe %Solution% /t:Build /p:Configuration=Release
+@SET Nuget=%Root%tools\nuget.exe
+@SET NugetFolder=%Root%packages
 
 @SET Project=LightInject
-%Nuget% pack src\%Project%\%Project%.nuspec -OutputDirectory %NugetFolder% -Properties Configuration=Release
+@SET Solution=src\%Project%.sln
 
-@Pause
+dotnet build %Solution% /t:Build /p:Configuration=Release
 
-%Nuget% push %NugetFolder%\LightInject.nupkg -ConfigFile ..\..\nuget.config -Source Nuget-EBerzosa -Verbosity detailed
+dotnet pack src\%Project%\%Project%.csproj --output %NugetFolder% --no-build
+
+@echo.
+@echo Press Control+C to abort NuGet Publish
+@echo.
+@Pause 
+
+%Nuget% push %NugetFolder%\%Project%.*.nupkg -ConfigFile ..\..\nuget.config -Source EBerzosa-NuGetPublish -Verbosity detailed
