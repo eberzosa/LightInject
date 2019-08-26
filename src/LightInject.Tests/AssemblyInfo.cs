bc118a63a4e7c93391016fa0dea9be084748ffe1
Enrique Berzosa <eberzosa@mimecast.com>
2019-08-26 10:52:46 +0100

    Fix double AssemblyInfo

--- src/LightInject.Tests/AssemblyInfo.cs
+++ src/LightInject.Tests/AssemblyInfo.cs
@@ -1,36 +1,7 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.CompilerServices;
 using LightInject;
 using LightInject.Tests;
 using Xunit;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4981e9dd-d57f-4191-8a70-65df6143da6c")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 
 [assembly: CompositionRootType(typeof(CompositionRootMock))]
 [assembly: InternalsVisibleTo("DynamicAssembly, PublicKey=002400000480000094000000060200000024000052534131000400000100010009a0524a3fac11a4dcd1c6e004d83287420b52b355c9b69c161af8481e52dbb17041bf8ae7c6a0f49e4c665fc4b040ef1974f12d25b8b225148db6e1cbd0ec65f147432cc0d33b6904c65821c4e9fc62b7f45d76533cfc43b60af58b25df370aa6c4b610efcb23d53320867ea1d48da1933583b4a1a71553f5711cf2017390eb")]
